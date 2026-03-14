3b54f415dc7f16bf53a2b8a9b66adc427bd3dfd3
markadeola <markadeola08@gmail.com>
2026-03-14 06:27:49 +0100

    just trying stuffs

---
+++ backendfile/App.js
@@ -0,0 +1,90 @@
+//common operation//
+// addition of two numbers//
+let a = 10;
+let b = 20;
+let sum = a + b;
+console.log(sum);
+//subtraction of two numbers//
+let c = 30;
+let d = 15;
+let difference = c - d;
+console.log(difference);
+// multiplication of two numbers//
+let e = 5;
+let f = 6;
+let product = e * f;
+console.log(product);
+// division of two numbers//
+let g = 20;
+let h = 4;
+let quotient = g / h;
+console.log(quotient);
+//shorthand addition//
+let i=10;
+let j=20;
+i+=j;
+console.log(i);
+//shorthand subtraction//   
+let k=30;
+let m=15;
+k-=m;
+console.log(k);
+//shorthand multiplication//    
+let n=5;
+let l=6;
+n*=l;
+console.log(n);   
+//shorthand division//
+let o=20;
+let p=4;
+o/=p;
+console.log(o);
+//increment//
+let girl=10;
+girl++;
+console.log(girl);
+//decrement//
+let boy=20;
+boy--;
+console.log(boy);
+//exponent//
+let q=2;
+let r=3;
+let exponent = q ** r;
+console.log(exponent);
+//remainder//
+let s=10;
+let t=3;
+let remainder = s % t;
+console.log(remainder);
+
+
+//comparison operators//
+//strict equality//
+let u=10;   
+let v=10;
+console.log(u === v);
+//non-strict equality//
+let w=10;
+let x="10";
+console.log(w == x);
+//strict non-equality//
+let y=10;
+let z=20;
+console.log(y !== z);
+//less than//
+let aa=5;
+let bb=10;  
+console.log(aa < bb);
+//greater than//
+let cc=15;
+let dd=10;
+console.log(cc > dd);
+//less than or equal to//
+let ee=5;
+let ff=5;
+console.log(ee <= ff);
+//greater than or equal to//
+let gg=10;
+let hh=10;
+console.log(gg >= hh);
